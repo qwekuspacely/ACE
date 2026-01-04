1449786722fdce39f9212b76e25c7afb46a60b71
qwekuspacely <achirigodfred@gmail.com>
2026-01-04 11:31:10 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   botOwner: 'SPACELY', // Your name
   ownerNumber: '233534163467', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
